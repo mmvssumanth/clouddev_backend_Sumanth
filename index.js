16cea7dd3db805074327275040b406d8f39f5622
mmvssumanth <146044369+mmvssumanth@users.noreply.github.com>
2025-04-18 14:25:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use(express.json());
 // MySQL connection config
 // triggering azure deployment
 const db = mysql.createConnection({
-  host: 'studentdb-demo.mysql.database.azure.com',
-  user: 'mysql_admin',
-  password: 'TempPass@123',
+  host: 'sumanth-mysql.mysql.database.azure.com',
+  user: 'mysql_sumanth',
+  password: 'Sumanth@98',
   database: 'studentdb',
   port: 3306,
   ssl: {
